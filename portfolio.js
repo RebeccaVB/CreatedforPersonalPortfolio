888b922b01c1e8d5a73f9f7cd253f4bb11319cd5
rebeccavb <r12vazquez@gmail.com>
2017-03-06 19:38:34 -0500

    functioning ajax

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,16 @@
-$(document).ready(funtion(){
-    $.getJSON('https://api.github.com/users/RebeccaVB', function(result){
-         $.each(repos, function( index, repo ) {
+$(document).ready(function(){
+    $.getJSON('https://api.github.com/users/RebeccaVB/repos', function(response){
+         for (var repoIdx = 0; repoIdx < response.length; repoIdx++) {
+            var repo = response[repoIdx];
+            var repoLink = repo.html_url;
+             var repoName = repo.name;
+            var repoUpdated = repo.updated_at;
             console.log(repo.name, repo.html_url, repo.updated_at, repo.subscriptions_url);
             $(".repo-list")
             .append("<li><a href=" + repo.html_url + ">" + repo.name + "<p>last update:" + repo.updated_at +"</p>" + "</a></li>");
-        });
+           
+                    };
     });
-    };
+    });
         
-     //   for(var repoIndx = 0; repoIndx < response.length; repoIndx++) {
-     //       var repo = response[repoIndx];
-      //      var name = repo.name;
-     //       var update = repo.update_at;
-     //       var link = repo.html_url
-      //  }
-  //  };
+    
