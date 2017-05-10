918f5b2c034c58e944fe5bd2868b4973d43d23b8
Rebecca Bernstiel <r12vazquez@gmail.com>
2017-05-10 13:19:51 -0400

    Font awesome

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,6 +12,15 @@ $(document).ready(function(){
                     };
     });
     });
+ 
+ $(document).ready(function() {
+  var $style;
+  $style = $('<style type="text/css">:before,:after{content:none !important}</style>');
+  $('head').append($style);
+  return setTimeout((function() {
+    return $style.remove();
+  }), 0);
+});
 
 $(function() {
         $('.material-card > .mc-btn-action').click(function () {
